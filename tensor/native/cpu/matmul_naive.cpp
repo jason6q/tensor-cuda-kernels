55bfbdeba2c33247bfe799d9719d8c8f73b9551d
nu1ptr <j6quach@gmail.com>
2025-11-11 23:54:33 -0600

    matmul cpu sig

--- tensor/native/cpu/matmul_naive.cpp
+++ tensor/native/cpu/matmul_naive.cpp
@@ -1,4 +1,9 @@
+#include <memory>
+#include <omp.h>
 
-void* matmul_kernel_cpu(){
-
+template <typename scalar_t>
+void matmul_kernel_cpu(const scalar_t* a, const scalar_t* b, scalar_t* c, int32_t m, int32_t k, int32_t n){
+    // Use OMP
 }
+
+template void matmul_kernel_cpu<float>(const float*, const float*, float*, int32_t m, int32_t k, int32_t n);
--- tensor/native/cpu_kernels.h
+++ tensor/native/cpu_kernels.h
@@ -2,3 +2,6 @@
 
 template<typename scalar_t>
 std::unique_ptr<scalar_t> arange_kernel_cpu(int32_t n);
+
+template<typename scalar_t>
+void matmul_kernel_cpu(const scalar_t* a, const scalar_t* b, scalar_t* c, int32_t m, int32_t k, int32_t n);
